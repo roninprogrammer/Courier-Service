ac78a4638e6e425bfd1f1b70ccfc0dcb2a801fb8
Vicknesh Balasubramaniam <roninprogrammer@gmail.com>
2025-03-09 11:56:51 +0800

    Added OfferCodes.java to utils package to ensure clean speration of constants from logic

---
+++ src/main/java/com/roninprogrammer/courierservice/utils/OfferCodes.java
@@ -0,0 +1,7 @@
+package com.roninprogrammer.courierservice.utils;
+
+public class OfferCodes {
+    public static final String OFR001 = "OFR001";
+    public static final String OFR002 = "OFR002";
+    public static final String OFR003 = "OFR003";
+}
